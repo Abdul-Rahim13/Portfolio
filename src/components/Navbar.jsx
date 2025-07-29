4ce49b22c79e2bd74d0ecc90a6cfad59f4cd794a
Abdul Rahim <raheemabdur106@gmail.com>
2025-07-29 23:40:27 +0500

    Updated App.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      <ul className='flex justify-around items-center h-16 list-none bg-[#161513] text-red-500'>
+      <ul className='flex justify-around items-center h-16 list-none bg-[#161513] text-white'>
         <li>Home</li>
         <li>About Me</li>
         <li>Services</li>
