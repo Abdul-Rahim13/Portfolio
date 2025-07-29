c2c03c28d0d6b1c5b397fa5eb0b8c3535bc98aec
Abdul Rahim <raheemabdur106@gmail.com>
2025-07-29 23:38:46 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul className='flex justify-around items-center h-16 list-none bg-[#161513] text-white'>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
