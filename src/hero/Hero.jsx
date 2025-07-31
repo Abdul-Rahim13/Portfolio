57d012069f065fa9637a31e514b079700f6ba74b
Abdul Rahim <raheemabdur106@gmail.com>
2025-07-31 23:11:37 +0500

    hero section chnages

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -4,9 +4,16 @@ import profile_image from '../assets/profile_picture.jpg'
 
 function Hero() {
   return (
-    <div>
-        <img src={profile_image} alt="" />
-        <h1>I'am Abdul Rahim, MERN Stack Developer based in Pakistan</h1>
+    <div className='flex flex-col items-center bg-[#161513] w-full top-0 left-0 m-0 p-0'>
+        <img className='h-[300px] w-[300px] mt-16 rounded-full' src={profile_image} alt="profile_image" />
+        <div className='mt-5'>
+            <h1 className='text-3xl font-semibold text-white'>I'am Abdul Rahim, MERN Stack</h1>
+            <h1 className='text-3xl font-semibold text-white'>Developer based in Pakistan</h1>
+        </div>
+        <div className='mt-5'>
+            <button className='text-white'>Connect with me</button>
+            <button className='text-white'>My resume</button>
+        </div>
     </div>
   )
 }
